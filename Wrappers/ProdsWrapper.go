ef146737c06fe53f43fccfe26f75f117461dbb6d
leiwen <657106593@qq.com>
2020-12-17 23:21:31 +0800

    feat；根据类型判断熔断服务

--- Wrappers/ProdsWrapper.go
+++ Wrappers/ProdsWrapper.go
@@ -15,7 +15,7 @@ type ProdsWrapper struct {
 func(this *ProdsWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error{
 	cmdName := req.Service()+"."+req.Endpoint()
 	configA := hystrix.CommandConfig{
-		Timeout:                1000,
+		Timeout:                2000,
 		MaxConcurrentRequests:  0,
 		RequestVolumeThreshold: 0,
 		SleepWindow:            0,
@@ -25,7 +25,8 @@ func(this *ProdsWrapper) Call(ctx context.Context, req client.Request, rsp inter
 	return hystrix.Do(cmdName, func() error {
 		return this.Client.Call(ctx,req,rsp)
 	}, func(e error) error {
-		defaultProds(rsp)
+		//defaultProds(rsp)
+		defaultData(rsp)
 		return nil
 	})
 }
@@ -39,6 +40,18 @@ func newProd(id int32,pname string) *Services.ProdModel {
 	return &Services.ProdModel{ProdID: id,ProdName:pname}
 }
 
+//通用降级方法
+func defaultData(rsp interface{}) {
+	switch t:=rsp.(type) {
+	case *Services.ProdListResponse:
+		defaultProds(rsp)
+	case *Services.ProdDetailResponse:
+		t.Data=newProd(10,"降级商品")
+
+	}
+}
+
+//商品列表降级方法
 func defaultProds(rsp interface{}) {
 	models := make([]*Services.ProdModel,0)
 	var i int32
